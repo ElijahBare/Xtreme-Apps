95c9f822e6d0d1ced797bb9853b67920955af74b
EBSmash <126611992+ElijahBare@users.noreply.github.com>
2023-12-16 15:57:57 -0500

    not working yet

--- predator/predator.c
+++ predator/predator.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "utils/subghz/subghz_txrx.h"
 #include "nfc/nfc_scanner.h"
-
+#include "utils/subghz/subghz_txrx_i.h"
 
 const int NumberOfApps = 2;
 
@@ -23,48 +23,49 @@ typedef struct {
 typedef struct {
     int selected_app_index;
     bool should_quit;
-    void* app_data; // Generic pointer for app-specific data
-} PredatorState;
+    SubScanAppContext sub_scan_app; // SubScanApp-specific data
+} PredatorAppState;
 
 void update_rssi(SubGhzTxRx* txrx, SubScanAppContext* context) {
-    context->current_rssi = subghz_txrx_radio_device_get_rssi(txrx); // Assuming this function exists
+    context->current_rssi = subghz_devices_get_rssi(txrx->radio_device);
 }
 
 void nfc_scanner_callback(NfcScannerEvent event, void* context) {
-    SubScanAppContext* app_context = (SubScanAppContext*)context;
+    PredatorAppState* app_state = (PredatorAppState*)context;
 
-    if(event.type == NfcScannerEventTypeDetected) {
-        // Assuming you want to store a summary of detected protocols
-        snprintf(app_context->nfc_protocol_info, sizeof(app_context->nfc_protocol_info),
+    if (event.type == NfcScannerEventTypeDetected && event.data.protocol_num > 0) {
+        snprintf(app_state->sub_scan_app.nfc_protocol_info, sizeof(app_state->sub_scan_app.nfc_protocol_info),
                  "Detected NFC Protocols: %zu", event.data.protocol_num);
+    } else {
+        strcpy(app_state->sub_scan_app.nfc_protocol_info, "No NFC detected");
     }
 }
 
-
-static void predator_app_draw(Canvas* canvas, PredatorState* state) {
+static void predator_app_draw(Canvas* canvas, PredatorAppState* state) {
     canvas_clear(canvas);
-
     canvas_draw_str(canvas, 10, 10, app_names[state->selected_app_index]);
 
-
     if (state->selected_app_index == 0) { // SubScanApp
-        SubScanAppContext* context = (SubScanAppContext*)state->app_data;
+        SubScanAppContext* context = &(state->sub_scan_app);
+        // Display RSSI
         char rssi_str[64];
         snprintf(rssi_str, sizeof(rssi_str), "RSSI: %.2f dBm", context->current_rssi);
-        canvas_draw_str(canvas, 10, 55, context->nfc_protocol_info);
+        canvas_draw_str(canvas, 10, 25, rssi_str);
+        // Display NFC protocol information
+        canvas_draw_str(canvas, 10, 40, context->nfc_protocol_info);
     }
 }
 
 static void predator_app_input_callback(InputEvent* input_event, void* context) {
-    PredatorState* state = context;
+    PredatorAppState* state = (PredatorAppState*)context;
 
-    if(input_event->type == InputTypePress) {
-        switch(input_event->key) {
+    if (input_event->type == InputTypePress) {
+        switch (input_event->key) {
             case InputKeyUp:
-                if(state->selected_app_index > 0) state->selected_app_index--;
+                if (state->selected_app_index > 0) state->selected_app_index--;
                 break;
             case InputKeyDown:
-                if(state->selected_app_index < NumberOfApps - 1) state->selected_app_index++;
+                if (state->selected_app_index < NumberOfApps - 1) state->selected_app_index++;
                 break;
             default:
                 state->should_quit = true;
@@ -73,26 +74,23 @@ static void predator_app_input_callback(InputEvent* input_event, void* context) 
     }
 }
 
-
-int predator_app() {
-    PredatorState state = {0};
+int sentinel_app() {
+    PredatorAppState state = {0};
     state.selected_app_index = 0;
     state.should_quit = false;
 
-    // Initialize app_data for SubScanApp
-    SubScanAppContext* app_context = malloc(sizeof(SubScanAppContext));
-    state.app_data = app_context;
+    state.sub_scan_app.current_rssi = 0.0;
+    state.sub_scan_app.current_rfid_strength = 0.0;
+    strcpy(state.sub_scan_app.nfc_protocol_info, "No NFC detected");
 
     SubGhzTxRx* txrx = subghz_txrx_alloc(); // Create SubGhzTxRx instance
     // Start the receiver
     subghz_txrx_rx_start(txrx);
-    
+
     Nfc* nfc = nfc_alloc(); // Assuming you have a way to allocate an Nfc instance
     NfcScanner* nfc_scanner = nfc_scanner_alloc(nfc);
 
-    nfc_scanner_start(nfc_scanner, nfc_scanner_callback, app_context);
-
-
+    nfc_scanner_start(nfc_scanner, nfc_scanner_callback, &state);
 
     Gui* gui = furi_record_open(RECORD_GUI);
     ViewPort* view_port = view_port_alloc();
@@ -100,18 +98,15 @@ int predator_app() {
     view_port_input_callback_set(view_port, predator_app_input_callback, &state);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    while(!state.should_quit) {
+    while (!state.should_quit) {
         if (state.selected_app_index == 0) {
             // Update RSSI for SubScanApp
-            update_rssi(txrx, (SubScanAppContext*)state.app_data);
+            update_rssi(txrx, &(state.sub_scan_app));
         }
-
         view_port_update(view_port);
-        furi_delay_ms(100); // Update every 100 ms, adjust as needed
     }
 
     // Cleanup
-    free(app_context);
     subghz_txrx_free(txrx);
     gui_remove_view_port(gui, view_port);
     view_port_free(view_port);
